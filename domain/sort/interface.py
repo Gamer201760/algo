c2705a17eef5e7edf00367f7957b4dfe8237a38e
Azamat <azamat201760@yandex.ru>
2025-11-23 12:43:48 +0300

    [add] interfaces

--- domain/sort/interface.py
+++ domain/sort/interface.py
@@ -3,5 +3,8 @@ from typing import Protocol
 
 class SortingAlgorithm(Protocol):
     @property
-    def name(self) -> str: ...
-    def sort(self, a: list[int]) -> list[int]: ...
+    def name(self) -> str:
+        raise NotImplementedError
+
+    def sort(self, a: list[int]) -> list[int]:
+        raise NotImplementedError
