b6a52ccfcaecf728be9861445e50a14d80d2a8bb
Azamat <azamat201760@yandex.ru>
2025-11-29 15:42:58 +0300

    [add] fibo, factorial

---
+++ domain/algo/factorial.py
@@ -0,0 +1,16 @@
+def factorial(n: int) -> int:
+    if n < 0:
+        raise ValueError('Факториал поддерживает только положительные n')
+
+    result = 1
+    for k in range(2, n + 1):
+        result *= k
+    return result
+
+
+def factorial_recursive(n: int) -> int:
+    if n < 0:
+        raise ValueError('Факториал поддерживает только положительные n')
+    if n in (0, 1):
+        return 1
+    return n * factorial_recursive(n - 1)
